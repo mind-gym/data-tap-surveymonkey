8e780f38661d40684afb98e6757e46e672f06d94
graham-jackson <79157558+graham-jackson@users.noreply.github.com>
2021-09-23 16:35:25 +0100

    DATA-399 Set STREAMS; format surveys.json

--- tap_surveymonkey/schema.py
+++ tap_surveymonkey/schema.py
@@ -3,23 +3,22 @@ import json
 from singer import metadata
 
 STREAMS = {
-    'responses': {
+    'surveys': {
         'key_properties': ['id'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'replication_keys': ['date_modified']
     },
     'simplified_responses': {
         'key_properties': ['id'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'replication_keys': ['date_modified']
     },
     'survey_details': {
         'key_properties': ['id'],
-        'replication_method': 'INCREMENTAL',
+        'replication_method': 'FULL_TABLE',
         'replication_keys': ['date_modified']
     }
 }
-
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
